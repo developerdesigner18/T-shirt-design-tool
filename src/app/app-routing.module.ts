a6ae0fb4450d2bd6cc5c7c263fe0c97053e4ced7
developerdesigner18 <developer.designer18@gmail.com>
2023-07-07 15:06:56 +0530

    change route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'first', pathMatch: 'full' },
-  { path: 'first', component: FirstComponent }
+  // { path: '', redirectTo: 'first', pathMatch: 'full' },
+  // { path: 'first', component: FirstComponent }
 ];
 
 @NgModule({
